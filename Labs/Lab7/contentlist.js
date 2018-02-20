7a32a3df786f636ca98ae4d8e518ed82bb9b0c77
mcsmall1 <mcsmall1@live.co.uk>
2018-02-20 10:42:19 +0000

    Task 3.b Q4

--- Labs/Lab7/contentlist.js
+++ Labs/Lab7/contentlist.js
@@ -2,7 +2,7 @@
 function addContent () {
 	// add a list of items to the content div
 	var items = ["hewey", "dewey", "louie"];
-	
+
 	// build the html string for a <ul> list
 	var items_html = "<ul>";
 	for (var i=0; i < items.length; i++) {
@@ -10,10 +10,9 @@ function addContent () {
 		items_html += "<li>" + item + "</li>";
 	};
 	items_html += "</ul>";
-	
+
 	// using javascript
 	// 1. find the content div
 	// 2. modify its html attribute by adding items_html
-
+	document.getElementById('content').innerHTML = items_html;
 }
-
