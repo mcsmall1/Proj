f97673de92d192556b3919df88fdfd9d958e311c
mcsmall1 <mcsmall1@live.co.uk>
2018-02-27 10:37:06 +0000

    27.02.18 Lab 10 pg15

---
+++ Labs/Lab10/getresources.js
@@ -0,0 +1,24 @@
+$(function(){
+  $('#btnGetReso').click(function(){
+    var dispResc = $('#dispResc');
+
+    dispResc.text('Loading data from JSON source..')
+  });
+
+  $.ajax({
+    type: "GET",
+    url: "resources.json",
+
+    success: function(result){
+      var output ="<table> <thead> <tr>  <th>Name</th>  <th>Provider</th>  <th>URL</th>  </thead> <tbody>";
+
+      for (var i in result){
+        output+="<tr><td>" + result[i].name + "</td><td>" + result[i].provider + "</td><td>" +
+        result[i].url + "</td></tr>";
+      }
+
+      output+="</tbody></table>";displayResources.html(output);
+      $("table").addClass("table");
+    };
+  });
+});
--- Labs/Lab9/mouseevents_jq.js
+++ Labs/Lab9/mouseevents_jq.js
@@ -1,18 +1,32 @@
 // JavaScript Document
 
 $(function(){
-	// usual main starting point when web page loads
-	
-	
-	// use jquery to find the div with id button1
-	// set the mouse enter and mouse leave functions, just like the onclick in the lecture
-	// use the html attribute to set the text
-	$("#button1").mouseenter(function(){
-		$("#button1").html("<p>Thank You</p>");
-	});
-	
-	// now you need to do the same for button1 for mouseleave
-	// and mouseup, mousedown for button2
-	
-});
 
+	$("#button1").mouseenter(
+	function()
+	{
+		$("#button1").html("<p>Thank You</p>")
+	});
+
+
+	$("#button1").mouseleave(
+		function()
+		{
+		$("#button1").html("<p>Mouse Over Here!</p>")
+	});
+
+
+	$("#button2").mouseup(
+		function()
+		{
+		$("#button2").css("background", "lightblue")
+	});
+
+
+	$("#button2").mousedown(
+		function()
+		{
+		$("#button2").css("background", "lightgreen");
+	});
+
+});
