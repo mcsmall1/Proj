d064906ed9e02817556e75df502f6ea074507bc2
mcsmall1 <mcsmall1@live.co.uk>
2018-03-01 10:25:00 +0000

    01.03.18 Lab 10 comp - Server Testing

--- Labs/Lab10/getresources.js
+++ Labs/Lab10/getresources.js
@@ -1,5 +1,6 @@
 $(function(){
-  $('#btnGetReso').click(function(){
+  $('#btnGetReso').click(function()
+  {
     var dispResc = $('#dispResc');
 
     dispResc.text('Loading data from JSON source..')
@@ -9,15 +10,17 @@ $(function(){
     type: "GET",
     url: "resources.json",
 
-    success: function(result){
+    success: function(result)
+    {
       var output ="<table> <thead> <tr>  <th>Name</th>  <th>Provider</th>  <th>URL</th>  </thead> <tbody>";
 
-      for (var i in result){
-        output+="<tr><td>" + result[i].name + "</td><td>" + result[i].provider + "</td><td>" +
-        result[i].url + "</td></tr>";
+      for (var i in result)
+      {
+        output+="<tr><td>" + result[i].name + "</td><td>" + result[i].provider + "</td><td>" + result[i].url + "</td></tr>";
       }
+      output+="</tbody></table>";
 
-      output+="</tbody></table>";displayResources.html(output);
+      dispResc.html(output);
       $("table").addClass("table");
     };
   });
