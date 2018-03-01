fe03a5fde25068cd37460b86cf4b4d4073f4c7b5
mcsmall1 <mcsmall1@live.co.uk>
2018-03-01 11:29:52 +0000

    ...

--- Labs/Lab10/getresources.js
+++ Labs/Lab10/getresources.js
@@ -2,8 +2,10 @@ $(function(){
   $('#btnGetReso').click(function()
   {
     var dispResc = $('#dispResc');
+    console.log("dispResc: "+ dispResc);
 
     dispResc.text('Loading data from JSON source..')
+    console.log("dispResc: "+ dispResc);
   });
 
   $.ajax({
@@ -13,15 +15,22 @@ $(function(){
     success: function(result)
     {
       var output ="<table> <thead> <tr>  <th>Name</th>  <th>Provider</th>  <th>URL</th>  </thead> <tbody>";
+      console.log("dispResc: "+ dispResc);
 
       for (var i in result)
       {
         output+="<tr><td>" + result[i].name + "</td><td>" + result[i].provider + "</td><td>" + result[i].url + "</td></tr>";
+        console.log("output: "+ output);
       }
       output+="</tbody></table>";
+      console.log("output: "+ output);
+      console.log("dispResc: "+ dispResc);
 
       dispResc.html(output);
       $("table").addClass("table");
+
+      console.log("FINAL - output: "+ output);
+      console.log("FINAL - dispResc: "+ dispResc);
     }
   });
 });
