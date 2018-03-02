2550e7d6836ce9776d6965f26e802f6f6c3fc449
LEWIS MCCULLOUGH (1702733) <1702733@rgu.ac.uk>
2018-03-02 11:36:55 +0000

    ...

--- Labs/Lab10/getresources.js
+++ Labs/Lab10/getresources.js
@@ -9,28 +9,28 @@ $(function(){
     console.log("Point ---- 2");
 
 
-  $.ajax({
-    type: "GET",
-    url: "resources.json",
+    $.ajax({
+      type: "GET",
+      url: "resources.json",
 
-    success: function(result)
-    {
-      console.log(result);
-      var output ="<table> <thead> <tr>  <th>Name</th>  <th>Provider</th>  <th>URL</th>  </thead> <tbody>";
-      console.log("Point ---- 3");
-
-      for (var i in result)
+      success: function(result)
       {
-        output+="<tr><td>" + result[i].name + "</td><td>" + result[i].provider + "</td><td>" + result[i].url + "</td></tr>";
-        console.log("Point ---- 4");
-      }
-      output+="</tbody></table>";
-      console.log("Point ---- 5");
-      divResults.html("<p> test</p>");
-      $("table").addClass("table");
-      console.log("Point ---- 6");
+        console.log(result);
+        var output ="<table> <thead> <tr>  <th>Name</th>  <th>Provider</th>  <th>URL</th>  </thead> <tbody>";
+        console.log("Point ---- 3");
 
-    }
-      });
+        for (var i in result)
+        {
+          output+="<tr><td>" + result[i].name + "</td><td>" + result[i].provider + "</td><td>" + result[i].url + "</td></tr>";
+          console.log("Point ---- 4");
+        }
+        output+="</tbody></table>";
+        console.log("Point ---- 5");
+
+        divResults.html(output);
+        $("table").addClass("table");
+        console.log("Point ---- 6");
+        }
+    });
   });
 });
