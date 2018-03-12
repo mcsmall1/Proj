fb890a2ba6ea430a362e130f1feaf21b3fdfb459
LEWIS MCCULLOUGH (1702733) <1702733@rgu.ac.uk>
2018-03-12 15:19:40 +0000

    12/03/18 lab 11 final script

--- Labs/Lab11/finalScript.js
+++ Labs/Lab11/finalScript.js
@@ -3,35 +3,26 @@ $(function() {
 
   $('#frmSearch').submit(function(){
     var searchterms = $("#searchterms").val();
-
-    addItem("searchterms");
-
+    getResultFromOMDB("searchterms");
     return false;
   });
 });
 
 function getResultFromOMDB(searchterms){
   console.log(searchterms);
-
   var url = "https://www.omdbapi.com/?i=tt3896198&apikey=d5f27ac3" + searchterms;
-
   $.getJSON(url, function(jsondata){
-    addResults(jsondata);
-
+    addResultsTitles(jsondata);
     console.log(jsondata);
   });
 }
 
-function addResults(jsondata){
+function addResultsTitles(jsondata){
   var htmlstring = "";
-
   for(var i=0; i<10; i++){
     var title = jsondata.Search[i].Title;
-
     htmlstring += "<li>" +title+ "</li>"
   };
-
   console.log(htmlstring);
-
   $('#divRes').append(htmlstring);
 };
