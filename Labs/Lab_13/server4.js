7304733940b5c9cfcda3070770ccc6a1387e65ea
mcsmall1 <mcsmall1@live.co.uk>
2018-03-18 19:26:55 +0000

    lab13 pg24

--- Labs/Lab_13/server4.js
+++ Labs/Lab_13/server4.js
@@ -11,4 +11,9 @@ app.get('/joke', function(req,res){
   res.end(randomJoke.body);
 });
 
+app.get('/add', function(req, res) {
+  var x = req.query.x;
+  var y = req.query.y;
+  res.send("X+Y="+(x+y));
+});
 app.listen(8080);
