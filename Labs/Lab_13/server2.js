8f110092123d73d17fac75edbce5fd56e201e068
mcsmall1 <mcsmall1@live.co.uk>
2018-03-18 18:45:49 +0000

    ...

---
+++ Labs/Lab_13/server2.js
@@ -0,0 +1,7 @@
+var http = require('http');
+var curdate = require('./module1');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write("the date today is:" +curdate.myDateTime()+ ".");
+  res.end('Hello World!');
+}).listen(8080);
