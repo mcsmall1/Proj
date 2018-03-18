30ee13d40ba5a7b02429bcd6a8c492030ec8a6e1
mcsmall1 <mcsmall1@live.co.uk>
2018-03-18 17:55:03 +0000

    Lab12 pg15

--- Labs/Lab_12/main.js
+++ Labs/Lab_12/main.js
@@ -1,8 +1,9 @@
 $(function(){
+  //make map
   var map = L.map('mapid').setView([0, 0],1);
-
+  //create gray base layer
   var Esri_WorldGrayCanvas = L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}', {attribution:'Tiles &copy; Esri &mdash; DeLorme, NAVTEQ', maxZoom: 16 });
-
+  //add to map
   Esri_WorldGrayCanvas.addTo(map)
 });
 
@@ -11,5 +12,20 @@ $('#Quake').click(function() {
   console.log("getting quakes");
   $.getJSON("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson", function(result) {
     console.log(result)
+    result.features.forEach(function(quake){
+      //get coords
+      var lng = quake.geometery.coordinates[0];
+      var lat = quake.geometery.coordinates[1];
+
+      //create dot
+      var circle = L.circle([lat, lng], 1, {
+        color: 'red',
+        opacity: 0,
+        fillColor: 'red',
+        fillOpacity: 0.8
+      })
+      //add to map
+      circle.addTo(map);
+    });
   });
 });
