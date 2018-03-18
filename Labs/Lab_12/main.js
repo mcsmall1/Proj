2afc364eea53c65e8a3cc05cda30d0355ea98ff9
mcsmall1 <mcsmall1@live.co.uk>
2018-03-18 17:40:56 +0000

    lAB12 pg13

--- Labs/Lab_12/main.js
+++ Labs/Lab_12/main.js
@@ -3,5 +3,13 @@ $(function(){
 
   var Esri_WorldGrayCanvas = L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}', {attribution:'Tiles &copy; Esri &mdash; DeLorme, NAVTEQ', maxZoom: 16 });
 
-  Esri_WorldGrayCanvas.addTo(map);
+  Esri_WorldGrayCanvas.addTo(map)
+});
+
+//when the button on is clicked
+$('#Quake').click(function() {
+  console.log("getting quakes");
+  $.getJSON("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson", function(result) {
+    console.log(result)
+  });
 });
