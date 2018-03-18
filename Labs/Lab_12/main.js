d65288f1f54225ba23b4e7744c10c265b0eeb2be
mcsmall1 <mcsmall1@live.co.uk>
2018-03-18 18:02:59 +0000

    ...

--- Labs/Lab_12/main.js
+++ Labs/Lab_12/main.js
@@ -1,6 +1,6 @@
 $(function(){
   //make map
-  var map = L.map('mapid').setView([0, 0],1);
+  global var map = L.map('mapid').setView([0, 0],1);
   //create gray base layer
   var Esri_WorldGrayCanvas = L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}', {attribution:'Tiles &copy; Esri &mdash; DeLorme, NAVTEQ', maxZoom: 16 });
   //add to map
