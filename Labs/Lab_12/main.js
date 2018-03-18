4b597e6fcdebf4b0868f8df30e489400a0d05acd
mcsmall1 <mcsmall1@live.co.uk>
2018-03-18 16:22:56 +0000

    18.03.18 Lab 12 pg11

--- Labs/Lab_12/main.js
+++ Labs/Lab_12/main.js
@@ -1,10 +1,3 @@
 $(function(){
   var map = L.map('mapid').setView([0, 0],1);
-
-  var Esri_WorldGrayCanvas = L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}', {
-    maxZoom: 16
-    attribution:'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ',
-  });
-
-  Esri_WorldGrayCanvas.addTo(map);
 });
