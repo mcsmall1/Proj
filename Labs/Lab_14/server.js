d6af1a8cc06882ae31b2e8253259d10266e1482d
LEWIS MCCULLOUGH (1702733) <1702733@rgu.ac.uk>
2018-03-19 15:34:37 +0000

    ...

--- Labs/Lab_14/server.js
+++ Labs/Lab_14/server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-var Twitter = require('twittter');
+var Twitter = require('twitter');
 
 var client = new Twitter({
   consumer_key:  'dtuCznqFiIlhXBh2acgQhphJn',
