1ebb4dbdb0bf2f4d5c221de978b59e7219da8015
LEWIS MCCULLOUGH (1702733) <1702733@rgu.ac.uk>
2018-03-19 15:32:28 +0000

    Lab14 pg15

--- Labs/Lab_14/server.js
+++ Labs/Lab_14/server.js
@@ -1,6 +1,13 @@
 var express = require('express');
 var app = express();
+var Twitter = require('twittter');
 
+var client = new Twitter({
+  consumer_key:  'dtuCznqFiIlhXBh2acgQhphJn'
+  consumer_secret: 'WnPM9eliD4LHsnjTFBHGjE76JqWE5Pmk9i09MKkQ3FQb0bjtS6'
+  access_token_key: '	1324957310-ijS6E2MdxqaI2653Djueb5qxFpzFOnvMc0oiLgJ'
+  access_token_secret: 'O0uCzsmPZ3k5aSo3dwWluv8ilIIstuprzpjdMm9eNJSLe'
+})
 
 app.use(express.static('public'))
 
