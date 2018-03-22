e3a5eae059f120a751c8bbee82e86dd2c2176b5e
LEWIS MCCULLOUGH (1702733) <1702733@rgu.ac.uk>
2018-03-22 10:03:05 +0000

    ...

--- Labs/Lab_15/server.js
+++ Labs/Lab_15/server.js
@@ -7,11 +7,21 @@ app.use(express.static('public'))
 
 var db;
 
-MongoClient.connect(url, function(error, database) {
-    if(error){
-      throw error;
-      console.log(error);
-    };
-    db = database;
+MongoClient.connect(url, function(err, datb) {
+    if(error){throw error;console.log(error);};
+    db = datb;
     app.listen(8080);
+});
+
+app.get('/all', function(req, res) {
+  db.collection('quotes').find().toArray(function(err, res){
+    if(error){throw error;console.log(error);};
+    for (var i = 0; i < res.length; i++) {
+      var output += "<div>"
+      output += "<h3>" +res[i].name+ "</h3>"
+      output += "<p>" +res[i].quote+ "</p>"
+      output += "<div>"
+    }
+    res.send(output)
+  });
 });
