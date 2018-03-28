37f8ebabe3ffb096995c9883abccbcc5179d0d87
mcsmall1 <mcsmall1@live.co.uk>
2018-03-28 10:41:49 +0100

    Lab17 pg19

--- Labs/Lab_17/pt1/server.js
+++ Labs/Lab_17/pt1/server.js
@@ -5,9 +5,25 @@ var app = express();
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 // use res.render to load up an ejs view file
+
+// // index page
+// app.get('/', function(req, res) {
+// res.render('pages/index');
+// });
+
 // index page
 app.get('/', function(req, res) {
-res.render('pages/index');
+  var drinks = [
+    { name: 'Bloody Mary', drunkness: 3 },
+    { name: 'Martini', drunkness: 5 },
+    { name: 'Scotch', drunkness: 10 }
+  ];
+  var tagline = "Any code of your own that you haven't looked at for six or more months might as well have been written by someone else.";
+
+  res.render('pages/index', {
+    drinks: drinks,
+    tagline: tagline
+  });
 });
 // about page
 app.get('/about', function(req, res) {
