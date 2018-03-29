f245b3fcccc39a0f2a682c6175134a67eca43c09
LEWIS MCCULLOUGH (1702733) <1702733@rgu.ac.uk>
2018-03-29 10:30:05 +0100

    Lab17 lol

--- Labs/Lab_17/pt2/server.js
+++ Labs/Lab_17/pt2/server.js
@@ -1,5 +1,5 @@
 const MongoClient = require('mongodb').MongoClient;
-const url = "mongodb://localhost:27017/star_wars_quotes";
+const url = "mongodb://localhost:27017/SWQuote";
 const express = require('express');
 const bodyParser = require('body-parser')
 const app = express();
